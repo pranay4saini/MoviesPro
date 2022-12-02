077a2c145a8ae23fcd9191494073f8d854f455fe
pranay <pranay4saini@gmail.com>
2022-12-02 16:16:01 +0530

    created recyclerview adapter

---
+++ app/src/main/java/com/pranay/moviespro/adapters/MovieAdapter.java
@@ -0,0 +1,79 @@
+package com.pranay.moviespro.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pranay.moviespro.R;
+import com.pranay.moviespro.databinding.MovieListItemBinding;
+import com.pranay.moviespro.model.Movie;
+import com.pranay.moviespro.view.MovieActivity;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private Context context;
+    private ArrayList<Movie> movieArrayList;
+
+    public MovieAdapter(Context context, ArrayList<Movie> movieArrayList) {
+        this.context = context;
+        this.movieArrayList = movieArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        MovieListItemBinding movieListItemBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                R.layout.movie_list_item,parent,false);
+        return new MovieViewHolder(movieListItemBinding);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = movieArrayList.get(position);
+        holder.movieListItemBinding.setMovie(movie);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieArrayList.size();
+    }
+
+
+    //View Holder class
+    public class MovieViewHolder extends RecyclerView.ViewHolder{
+
+        private MovieListItemBinding movieListItemBinding;
+
+        public MovieViewHolder( MovieListItemBinding movieListItemBinding) {
+            super(movieListItemBinding.getRoot());
+            this.movieListItemBinding = movieListItemBinding;
+
+
+            movieListItemBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    if(position!=RecyclerView.NO_POSITION){
+                        Movie selectedMovie = movieArrayList.get(position);
+
+                        Intent i = new Intent(context, MovieActivity.class);
+                        i.putExtra("movie",selectedMovie);
+                        context.startActivity(i);
+                    }
+                }
+            });
+        }
+
+
+    }
+}
